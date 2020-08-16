48e81ca8ac4f2301f14313433ed918f6fe914b07
RyanStmr <ryan.s@hotmail.de>
2020-08-16 11:46:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// create an express app
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// use the express-static middleware
+app.use("/", express.static(path.join(__dirname, "Website")));
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
